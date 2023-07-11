88754f876a4d9297251a964c392babd2fa527f78
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-07-11 21:24:20 +0300

    fixed identity transfer 32 byte all values

--- x/rarimocore/crypto/pkg/op_identity_default_transfer.go
+++ x/rarimocore/crypto/pkg/op_identity_default_transfer.go
@@ -19,19 +19,19 @@ func GetIdentityDefaultTransfer(operation types.Operation) (*types.IdentityDefau
 func GetIdentityDefaultTransferContent(op *types.IdentityDefaultTransfer) (*operation.IdentityDefaultTransferContent, error) {
 	return &operation.IdentityDefaultTransferContent{
 		Contract:                 hexutil.MustDecode(op.Contract),
-		GISTHash:                 hexutil.MustDecode(op.GISTHash),
+		GISTHash:                 operation.To32Bytes(hexutil.MustDecode(op.GISTHash)),
 		Id:                       operation.To32Bytes(hexutil.MustDecode(op.Id)),
-		StateHash:                hexutil.MustDecode(op.StateHash),
+		StateHash:                operation.To32Bytes(hexutil.MustDecode(op.StateHash)),
 		StateCreatedAtTimestamp:  operation.To32Bytes(operation.AmountBytes(op.StateCreatedAtTimestamp)),
 		StateCreatedAtBlock:      operation.To32Bytes(operation.AmountBytes(op.StateCreatedAtBlock)),
 		StateReplacedAtTimestamp: operation.To32Bytes(operation.AmountBytes(op.StateReplacedAtTimestamp)),
 		StateReplacedAtBlock:     operation.To32Bytes(operation.AmountBytes(op.StateReplacedAtBlock)),
-		StateReplacedBy:          hexutil.MustDecode(op.StateReplacedBy),
-		GISTReplacedBy:           hexutil.MustDecode(op.GISTReplacedBy),
+		StateReplacedBy:          operation.To32Bytes(hexutil.MustDecode(op.StateReplacedBy)),
+		GISTReplacedBy:           operation.To32Bytes(hexutil.MustDecode(op.GISTReplacedBy)),
 		GISTCreatedAtTimestamp:   operation.To32Bytes(operation.AmountBytes(op.GISTCreatedAtTimestamp)),
 		GISTCreatedAtBlock:       operation.To32Bytes(operation.AmountBytes(op.GISTCreatedAtBlock)),
 		GISTReplacedAtTimestamp:  operation.To32Bytes(operation.AmountBytes(op.GISTReplacedAtTimestamp)),
 		GISTReplacedAtBlock:      operation.To32Bytes(operation.AmountBytes(op.GISTReplacedAtBlock)),
-		ReplacedStateHash:        hexutil.MustDecode(op.ReplacedStateHash),
+		ReplacedStateHash:        operation.To32Bytes(hexutil.MustDecode(op.ReplacedStateHash)),
 	}, nil
 }
