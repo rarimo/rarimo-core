77fb3477abec9064d2bfd3064cef263cf08754af
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-09-20 17:18:17 +0300

    adding comments for identity module

--- x/identity/keeper/abci.go
+++ x/identity/keeper/abci.go
@@ -8,6 +8,7 @@ import (
 
 func (k Keeper) EndBlocker(ctx sdk.Context) {
 	params := k.GetParams(ctx)
+	// Check is there any states that was changed during that block
 	if len(params.StatesWaitingForSign) == 0 {
 		// Nothing to do
 		return
@@ -18,6 +19,7 @@ func (k Keeper) EndBlocker(ctx sdk.Context) {
 		return
 	}
 
+	// Creating operation to be signed by TSS parties
 	index, err := k.rarimo.CreateIdentityAggregatedTransferOperation(ctx, types.ModuleName, &rarimocoremoduletypes.IdentityAggregatedTransfer{
 		Contract:      params.IdentityContractAddress,
 		Chain:         params.ChainName,
@@ -31,6 +33,7 @@ func (k Keeper) EndBlocker(ctx sdk.Context) {
 		return
 	}
 
+	// Saving operation index for any state that was changed during that block
 	for _, id := range params.StatesWaitingForSign {
 		state, ok := k.GetStateInfo(ctx, id)
 		if ok {
@@ -39,6 +42,7 @@ func (k Keeper) EndBlocker(ctx sdk.Context) {
 		}
 	}
 
+	// Resetting list of changed states
 	params.StatesWaitingForSign = []string{}
 	k.SetParams(ctx, params)
 }
--- x/identity/keeper/rnd.go
+++ x/identity/keeper/rnd.go
@@ -6,6 +6,10 @@ type LCG struct {
 	Keeper
 }
 
+// Next function returns the next pseudo-random value depending on generator state in module params.
+// Such simple solution is claimed to be enough for balancing binary tree nodes.
+// Linear congruential generator
+// More about: https://en.wikipedia.org/wiki/Linear_congruential_generator
 func (l LCG) Next(ctx sdk.Context) uint64 {
 	params := l.GetParams(ctx)
 	params.LcgValue = (params.LcgA*params.LcgValue + params.LcgB) % params.LcgMod
