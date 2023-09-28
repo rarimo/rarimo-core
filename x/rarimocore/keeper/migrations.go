49404531e5d455961a068e6cc966708d88e92688
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2023-09-28 11:27:36 +0300

    adding migration for rarimocore module (removing 0x04 prefix from pubkeys)

---
+++ x/rarimocore/keeper/migrations.go
@@ -0,0 +1,24 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	v2 "gitlab.com/rarimo/rarimo-core/x/rarimocore/migrations/v3"
+)
+
+type Migrator struct {
+	keeper Keeper
+}
+
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{
+		keeper: keeper,
+	}
+}
+
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	if err := v2.MigrateStore(ctx, m.keeper.storeKey, m.keeper.cdc); err != nil {
+		return err
+	}
+
+	return nil
+}
