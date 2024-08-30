8c5d15521da143086be4733fe57946043b6fc87b
Mark Cherepovskyi <mark.cherepovskyi@gmail.com>
2024-08-30 13:47:26 +0300

    logs and update handler

--- x/rootupdater/keeper/keeper.go
+++ x/rootupdater/keeper/keeper.go
@@ -6,6 +6,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -57,39 +58,53 @@ func NewKeeper(
 func (k Keeper) PostTxProcessing(ctx sdk.Context, msg core.Message, receipt *ethtypes.Receipt) error {
 	params := k.GetParams(ctx)
 
+	k.Logger(ctx).Error("PostTxProcessing", "msg", msg, "receipt", receipt)
+
 	stateV2, err := abi.JSON(strings.NewReader(state.PoseidonSMTABI))
 	if err != nil {
+		k.Logger(ctx).Error("failed to marshal poseidon smart abi", "error", err)
 		return err
 	}
 
 	contractAddress, err := hexutil.Decode(params.ContractAddress)
 	if err != nil {
 		// If return an error here, the whole EVM module won't work
-		k.Logger(ctx).Debug("failed to decode contract address")
+		k.Logger(ctx).Info("failed to decode contract address")
 		return nil
 	}
 
 	// Validating message receiver address (should be our state smart contract)
 	if msg.To() == nil || bytes.Compare(msg.To().Bytes(), contractAddress) != 0 {
+		k.Logger(ctx).Info("inappropriate contract address")
 		return nil
 	}
 
+	var commonError error
 	// https://docs.evmos.org/protocol/modules/evm#posttxprocessing
+
+	if len(receipt.Logs) == 0 {
+		k.Logger(ctx).Error("logs is empty")
+	}
+
 	for _, log := range receipt.Logs {
 		eventId := log.Topics[0]
-
 		event, err := stateV2.EventByID(eventId)
 		if err != nil {
+			k.Logger(ctx).Error("failed to get event by ID")
+			commonError = errors.Wrap(commonError, errors.Wrapf(err, "failed to get event by id %s", eventId).Error())
 			continue
 		}
 
 		if event.Name != params.EventName {
+			k.Logger(ctx).Info("unmatched event: got %s, expected %s", event.Name, params.EventName)
 			continue
 		}
 
 		eventBody := state.PoseidonSMTRootUpdated{}
 		if err := utils.UnpackLog(stateV2, &eventBody, event.Name, log); err != nil {
-			return err
+			k.Logger(ctx).Error("failed to unpack event body")
+			commonError = errors.Wrap(commonError, errors.Wrapf(err, "failed to unpack event %s", eventId).Error())
+			continue
 		}
 
 		params.Root = hexutil.Encode(eventBody.Root[:])
@@ -100,7 +115,7 @@ func (k Keeper) PostTxProcessing(ctx sdk.Context, msg core.Message, receipt *eth
 		k.SetParams(ctx, params)
 	}
 
-	return nil
+	return commonError
 }
 
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
