b6a94158bdf9c0fd66b47e5a3ca73f1143164e82
Mark Cherepovskyi <mark.cherepovskyi@gmail.com>
2024-09-03 11:35:31 +0300

    update handler v1.1.4-rc5

--- app/app.go
+++ app/app.go
@@ -1154,6 +1154,13 @@ func New(
 		},
 	)
 
+	app.UpgradeKeeper.SetUpgradeHandler(
+		"v1.1.4-rc5",
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+
 	if loadLatest {
 		if err := app.LoadLatestVersion(); err != nil {
 			tmos.Exit(err.Error())
